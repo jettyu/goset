867435c2ff13f042e986094fa364af2550a87871
FishFish <fishfish@FishFishdeMacBook-Pro.local>
2018-03-15 18:23:33 +0800

    add safeset

---
+++ safe_set.go
@@ -0,0 +1,73 @@
+package goset
+
+import "sync"
+
+type safeSet struct {
+	set Set
+	sync.RWMutex
+}
+
+var _ Set = (*safeSet)(nil)
+
+// NewSafeSet ...
+func NewSafeSet(set Set) Set {
+	return &safeSet{
+		set: set,
+	}
+}
+
+func (p *safeSet) Has(v interface{}, pos int) bool {
+	p.RLock()
+	ok := p.set.Has(v, pos)
+	p.RUnlock()
+	return ok
+}
+
+func (p *safeSet) Insert(v Items) int {
+	p.Lock()
+	n := p.set.Insert(v)
+	p.Unlock()
+	return n
+}
+
+func (p *safeSet) Erase(v Items) int {
+	p.Lock()
+	n := p.set.Erase(v)
+	p.Unlock()
+	return n
+}
+
+func (p *safeSet) Items() Items {
+	p.RLock()
+	it := p.set.Items()
+	p.RUnlock()
+	return it
+}
+
+func (p *safeSet) Value() interface{} {
+	p.RLock()
+	v := p.set.Value()
+	p.RUnlock()
+	return v
+}
+
+func (p *safeSet) Search(v interface{}, pos int) int {
+	p.RLock()
+	i := p.set.Search(v, pos)
+	p.RUnlock()
+	return i
+}
+
+func (p *safeSet) Equal(v Items) bool {
+	p.RLock()
+	ok := p.set.Equal(v)
+	p.RUnlock()
+	return ok
+}
+
+func (p *safeSet) Get(v interface{}) (data interface{}, ok bool) {
+	p.RLock()
+	data, ok = p.set.Get(v)
+	p.RUnlock()
+	return
+}
